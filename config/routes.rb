87fa084157088cda9b2d75be26027f1c7fd8f0b8
kenttoshima <hakuaki136@icloud.com>
2018-06-19 17:21:08 +0900

    set groups routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update, :show]
   devise_for :users
   root to: 'messages#index'
 end
