37cb59e09fc6a32e63b0ed4f657f12ed0dadad0d
kenttoshima <hakuaki136@icloud.com>
2018-06-19 16:51:47 +0900

    edited migration for create_group and create_group_user

--- db/migrate/20180619074141_create_user_groups.rb
+++ db/migrate/20180619074141_create_user_groups.rb
@@ -1,7 +1,8 @@
 class CreateUserGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :user_groups do |t|
-
+      t.reference :group, foreign_key: true
+      t.reference :user, foreign_key: true
       t.timestamps
     end
   end
