ec2473baaaebee55959e9072faf40d270e60e2bc
kenttoshima <hakuaki136@icloud.com>
2018-06-21 12:34:16 +0900

    edit migrate file for message

--- db/migrate/20180621032952_create_messages.rb
+++ db/migrate/20180621032952_create_messages.rb
@@ -1,7 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-
+      t.text :message_body
+      t.text :image
+      t.refrences :group, null: false, foreign_key: true
+      t.refrences :user, null: false, foreign_key: true
       t.timestamps
     end
   end
