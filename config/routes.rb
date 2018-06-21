bd2bc8445c71792fa3fbdb8912b87436338c91be
kenttoshima <hakuaki136@icloud.com>
2018-06-21 10:23:33 +0900

    change file tree

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update, :show]
   devise_for :users
-  root to: 'messages#index'
+  root to: 'groups#index'
 end
