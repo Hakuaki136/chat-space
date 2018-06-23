6b2dc3505e44671bc494d2fda601e74474ba3e6f
kenttoshima <hakuaki136@icloud.com>
2018-06-23 12:51:47 +0900

    finish side-bar show_last_message

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,13 @@ class Group < ApplicationRecord
   has_many :user_groups
   has_many :users, through: :user_groups
   validates :name, presence: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.message_body? ? last_message.message_body : '画像が投稿されています'
+    else
+      'まだメッセージが存在しません'
+    end
+
+  end
 end
