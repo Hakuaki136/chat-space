605a445f241f75857a0d0d92cd57adead4a9f15b
kenttoshima <hakuaki136@icloud.com>
2018-06-29 09:33:00 +0900

    implement appendMessage

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,22 @@
 $(function() {
+  var message_list = $('.chat-main__body--messages-list');
+
+  function appendMessage(message) {
+    let html = `<div class="chat-main__message clearfix">
+                  <div class="chat-main__message-name">
+                    ${ message.user_name }
+                  </div>
+                  <div class="chat-main__message-time">
+                    ${ message.created_at }
+                  </div>
+                  <div class="chat-main__message-body">
+                    message.message_body
+                    <img src="${ message.image_url }" alt="">
+                  </div>
+                </div>`
+    message_list.append(html);
+  }
+
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var messageData = new FormData(this);
@@ -10,6 +28,9 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(message) {
+      appendMessage
     })
   })
 })
