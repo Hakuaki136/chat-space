a351a3eccc5b3f361767a87830e9026e1c33964e
kenttoshima <hakuaki136@icloud.com>
2018-07-01 18:40:05 +0900

    fix class in user_search

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -3,7 +3,7 @@ $(function() {
   var member_list = $('#chat-group-users');
   var preWord;
   function appendUser(user) {
-    let html = `<div class="chat-group-user clearfix">
+    let html = `<div class="chat-group-user js-chat-member clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
                 </div>`;
