89f8aa83253968c034043d74bc6bbce6d4633b23
kenttoshima <hakuaki136@icloud.com>
2018-07-01 18:41:28 +0900

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:edit, :update, :index]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update, :index, :show] do
     resources :messages, only: [:index, :create]
   end
