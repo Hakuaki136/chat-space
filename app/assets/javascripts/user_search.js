9a711d0ff13758423ccb41043dcc9cef4f850e79
kenttoshima <hakuaki136@icloud.com>
2018-07-01 12:27:17 +0900

    implement appendMember

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,12 +1,21 @@
 $(function() {
   var search_list = $('#user-search-result')
+  var member_list = $('.chat-group-form__field--right')
   var user_ids = []
   function appendUser(user) {
     let html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
-                </div>`
-    search_list.append(html)
+                </div>`;
+    search_list.append(html);
+  }
+  function appendMember(user) {
+    let html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${ user.id }'>
+                  <p class='chat-group-user__name'>${ user.name }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`;
+    member_list.append(html);
   }
   $('#user-search-field').on('keyup', function() {
     var input = $('#user-search-field').val();
@@ -33,5 +42,6 @@ $(function() {
     user_ids.push($(this).data('userId'));
     console.log(user_ids);
     $(this).parents('.chat-group-user').remove();
+    $('.chat-group-form__field--right').;
   })
 })
