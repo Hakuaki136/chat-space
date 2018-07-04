9860133e4d676d1d697293ce71ffd91a65ee04cf
kenttoshima <hakuaki136@icloud.com>
2018-07-04 18:27:25 +0900

    jbuilder fix

--- app/assets/javascripts/load_message.js
+++ app/assets/javascripts/load_message.js
@@ -3,5 +3,8 @@ $(function() {
     $.ajax({
       url: location.href.json
     })
+    .done(function(messages) {
+
+    })
   }, 5000);
 });
--- app/views/messages/create.json.builder
+++
@@ -1,3 +0,0 @@
-message.user_name @message.user.name
-message.created_at @message.created_at
-message.image_url @message.image.url
