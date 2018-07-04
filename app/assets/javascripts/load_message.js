23379de37c99bdb37fa596d2e69e378d2759b389
kenttoshima <hakuaki136@icloud.com>
2018-07-04 18:34:28 +0900

    ajax.done

--- app/assets/javascripts/load_message.js
+++ app/assets/javascripts/load_message.js
@@ -4,7 +4,9 @@ $(function() {
       url: location.href.json
     })
     .done(function(messages) {
-
+      messages.forEach(function(message) {
+        appendMessage(message);
+      });
     })
   }, 5000);
 });
--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -3,18 +3,20 @@ $(function() {
   var member_list = $('#chat-group-users');
   var preWord;
   function appendUser(user) {
-    let html = `<div class="chat-group-user js-chat-member clearfix">
-                  <p class="chat-group-user__name">${ user.name }</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
-                </div>`;
+    let html = `
+      <div class="chat-group-user js-chat-member clearfix">
+        <p class="chat-group-user__name">${ user.name }</p>
+        <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+      </div>`;
     search_list.append(html);
   }
   function appendMember(user) {
-    let html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ user.data('userId') }'>
-                  <input name='group[user_ids][]' type='hidden' value='${ user.data('userId') }'>
-                  <p class='chat-group-user__name'>${ user.data('userName') }</p>
-                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-                </div>`;
+    let html = `
+      <div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ user.data('userId') }'>
+        <input name='group[user_ids][]' type='hidden' value='${ user.data('userId') }'>
+        <p class='chat-group-user__name'>${ user.data('userName') }</p>
+        <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+      </div>`;
     member_list.append(html);
   }
   function removeMember(button) {
