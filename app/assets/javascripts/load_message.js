f6dfd3f57aa08165615b75fc8fef3b8b8974e58f
kenttoshima <hakuaki136@icloud.com>
2018-07-24 17:20:54 +0900

    js added turbolinks to every files

--- app/assets/javascripts/load_message.js
+++ app/assets/javascripts/load_message.js
@@ -1,44 +1,46 @@
-$(function() {
-  const INTERVAL = 5000
-  var message_list = $('.chat-main__body--messages-list');
-  function buildHTML(message) {
-    let html = `
-      <div class="chat-main__message clearfix" data-id="${ message.id }">
-        <div class="chat-main__message-name">
-          ${ message.name }
-        </div>
-        <div class="chat-main__message-time">
-          ${ message.date }
-        </div>
-        <div class="chat-main__message-body">
-          ${ message.body }`;
-    let image = message.image ? `<img src="${ message.image }" alt="">` : ``;
-    return html + image + `</div></div>`;
-  };
+$(document).on('turbolinks:load', function() {
+  $(function() {
+    const INTERVAL = 5000
+    var message_list = $('.chat-main__body--messages-list');
+    function buildHTML(message) {
+      let html = `
+        <div class="chat-main__message clearfix" data-id="${ message.id }">
+          <div class="chat-main__message-name">
+            ${ message.name }
+          </div>
+          <div class="chat-main__message-time">
+            ${ message.date }
+          </div>
+          <div class="chat-main__message-body">
+            ${ message.body }`;
+      let image = message.image ? `<img src="${ message.image }" alt="">` : ``;
+      return html + image + `</div></div>`;
+    };
 
-  var reloading =
-        setInterval(function() {
-          if (window.location.href.match(/\/groups\/\d+\/messages/)) {
-            $.ajax({
-              type: 'GET',
-              url: window.location.href,
-              dataType: 'json'
-            })
-            .done(function(messages) {
-              let html = ``;
-              let newestId = $('.chat-main__message').data('id');
-              messages.forEach(function(message) {
-                if (newestId < message.id) {
-                  html += buildHTML(message);
-                }
+    var reloading =
+          setInterval(function() {
+            if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+              $.ajax({
+                type: 'GET',
+                url: window.location.href,
+                dataType: 'json'
+              })
+              .done(function(messages) {
+                let html = ``;
+                let newestId = $('.chat-main__message').data('id');
+                messages.forEach(function(message) {
+                  if (newestId < message.id) {
+                    html += buildHTML(message);
+                  }
+                });
+                message_list.prepend(html);
+              })
+              .fail(function() {
+                alert('自動更新に失敗しました');
               });
-              message_list.prepend(html);
-            })
-            .fail(function() {
-              alert('自動更新に失敗しました');
-            });
-          } else {
-            clearInterval(reloading);
-          }
-        }, INTERVAL);
-});
+            } else {
+              clearInterval(reloading);
+            }
+          }, INTERVAL);
+  });  
+}
--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,43 +1,45 @@
-$(function() {
-  var message_list = $('.chat-main__body--messages-list');
+$(document).on('turbolinks:load', function() {
+  $(function() {
+    var message_list = $('.chat-main__body--messages-list');
 
-  function appendMessage(message) {
-    let html = `<div class="chat-main__message clearfix">
-                  <div class="chat-main__message-name">
-                    ${ message.user_name }
-                  </div>
-                  <div class="chat-main__message-time">
-                    ${ message.created_at }
-                  </div>
-                  <div class="chat-main__message-body">
-                    ${ message.message_body }`
-    let image = message.image_url ? `<img src="${ message.image_url }" alt="">` : ``
-    html += image + `</div></div>`
-    message_list.prepend(html);
-  }
+    function appendMessage(message) {
+      let html = `<div class="chat-main__message clearfix">
+                    <div class="chat-main__message-name">
+                      ${ message.user_name }
+                    </div>
+                    <div class="chat-main__message-time">
+                      ${ message.created_at }
+                    </div>
+                    <div class="chat-main__message-body">
+                      ${ message.message_body }`
+      let image = message.image_url ? `<img src="${ message.image_url }" alt="">` : ``
+      html += image + `</div></div>`
+      message_list.prepend(html);
+    }
 
-  function scrollToTop(element) {
-    $(element).animate({scrollTop: $(this).offset().top});
-  }
+    function scrollToTop(element) {
+      $("html,body").animate({scrollTop: $(element).offset().top});
+    }
 
-  $('#new_message').on('submit', function(e) {
-    e.preventDefault();
-    var messageData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: 'POST',
-      data: messageData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(message) {
-      appendMessage(message);
-      scrollToTop(".chat-main__body--messages-list");
-    })
-    .fail(function() {
-      alert('メッセージの通信に失敗しました');
+    $('#new_message').on('submit', function(e) {
+      e.preventDefault();
+      var messageData = new FormData(this);
+      var url = $(this).attr('action')
+      $.ajax({
+        url: url,
+        type: 'POST',
+        data: messageData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(message) {
+        appendMessage(message);
+        scrollToTop(".chat-main__body--messages-list");
+      })
+      .fail(function() {
+        alert('メッセージの通信に失敗しました');
+      })
     })
   })
-})
+}
